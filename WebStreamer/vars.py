b5e3045d622dd068b3b2f335bf30eee541c0e549
xty9 <acc22free@gmail.com>
2024-11-28 14:34:30 -0300

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,25 +8,25 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("28645883"))
+    API_HASH = str(environ.get("e2d5ad008a5245627ac130ac63e2ecfd"))
+    BOT_TOKEN = str(environ.get("7192200134:AAEU7aW_SzubL_ZIVP6txlK59xwlj4tbtfM"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("-1002267274906", None)
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(environ.get("FQDN", BIND_ADDRESS))
+    FQDN = str(environ.get("150.136.63.174", BIND_ADDRESS))
     URL = "http{}://{}{}/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    DATABASE_URL = str(environ.get('DATABASE_URL'))
+    DATABASE_URL = str(environ.get('mongodb+srv://jvictor26in:aMUzXIcgf2EJ1PWZ@cluster0.bp1zg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "Telegram"))
     OWNER_ID = int(environ.get('OWNER_ID', '777000'))
     SESSION_NAME = str(environ.get('SESSION_NAME', 'F2LxBot'))
